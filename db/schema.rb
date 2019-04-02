828ab9dc10aa99594c862c3e8f27ad4d1d04c7e3
github username <github email address>
2019-04-02 09:38:49 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 3) do
     t.text "name"
     t.text "location"
     t.integer "costume_inventory"
-    t.integer "number_of_employees"
-    t.boolean "in_business"
-    t.integer "opening_time"
-    t.integer "closing_time"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -37,9 +37,9 @@ ActiveRecord::Schema.define(version: 3) do
     t.text "theme"
     t.integer "price"
     t.boolean "family_friendly"
-    t.integer "opening_time"
-    t.integer "closing_time"
-    t.text "long_description"
+    t.text "opening_date"
+    t.text "closing_date"
+    t.text "description"
   end
 
 end
